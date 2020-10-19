93f8d38bb44cc017f55bcc0a3cb8e4e7f57837d4
Vadim Eremenko <eremenko.v.o@yandex.ru>
2020-10-19 11:33:44 +0300

    init project

---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name]",
+          publicPath: "assets",
+        },
+      },
+    ],
+  },
+};
