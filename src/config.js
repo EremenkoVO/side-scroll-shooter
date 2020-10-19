07511484051452a00ad2db844cf57533473d2978
Vadim Eremenko <eremenko.v.o@yandex.ru>
2020-10-19 11:58:33 +0300

    create background

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 import Phaser from "phaser";
+import { GameScene } from "./gamescene";
 
 export var config = {
   type: Phaser.AUTO,
   width: 1280,
   height: 720,
-  scene: [],
+  scene: GameScene,
 };
